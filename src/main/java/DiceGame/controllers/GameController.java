b199e34b79db5bee3a6196dba678fbb975ab12e0
GuidoJops <guidojanota@gmail.com>
2024-01-02 20:34:22 +0100

    GameControllerTest done

--- src/main/java/DiceGame/controllers/GameController.java
+++ src/main/java/DiceGame/controllers/GameController.java
@@ -5,12 +5,10 @@ import DiceGame.services.IGameService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/games")
 public class GameController {
 
 	private final IGameService gameService;
@@ -19,14 +17,14 @@ public class GameController {
 		this.gameService = gameService;
 	}
 
-	@PostMapping("players/{id}/games")
+	@PostMapping("/{id}")
 	@PreAuthorize("#id == principal.id")
 	public ResponseEntity<GameDto> playGame(@PathVariable String id) {
 		GameDto gameDto = gameService.newGame(id);
         return new ResponseEntity<>(gameDto, HttpStatus.CREATED);
 	}
 	
-	@DeleteMapping("players/{id}/games")
+	@DeleteMapping("/{id}")
 	@PreAuthorize("hasRole ('ADMIN')")
 	public ResponseEntity<String> deleteGames(@PathVariable String id) {
 		gameService.deleteAllGamesByPlayerId(id);
---
+++ src/test/java/DiceGame/controllers/GameControllerTest.java
@@ -0,0 +1,69 @@
+package DiceGame.controllers;
+
+import DiceGame.model.dto.GameDto;
+import DiceGame.model.exceptions.PlayerNotFoundException;
+import DiceGame.services.IGameService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = GameController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class GameControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private IGameService gameService;
+
+
+    @Test
+    void shouldCreateGame() throws Exception {
+        GameDto gameDto = GameDto.builder()
+                .diceA(2)
+                .diceB(3)
+                .win(false)
+                .build();
+
+        when(gameService.newGame(anyString())).thenReturn(gameDto);
+
+        mockMvc.perform(post("/games/id")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(gameDto)))
+                .andExpect(jsonPath("$.diceA").value(gameDto.getDiceA()))
+                .andExpect(jsonPath("$.diceB").value(gameDto.getDiceB()))
+                .andExpect(jsonPath("$.win").value(gameDto.isWin()))
+                .andExpect(status().isCreated());
+
+        verify(gameService, times(1)).newGame(anyString());
+    }
+
+    @Test
+    void shouldDeleteAllPlayerGames() throws Exception {
+        doNothing().when(gameService).deleteAllGamesByPlayerId(anyString());
+
+        mockMvc.perform(delete("/games/id")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(gameService, times(1)).deleteAllGamesByPlayerId(anyString());
+    }
+}
