5d2e7540a843c155f4437cf705a114e00fd96dcd
GuidoJops <guidojanota@gmail.com>
2024-01-05 21:46:43 +0100

    PlayerRepositoryTest done

--- src/test/java/DiceGame/repository/PlayerRepositoryTest.java
+++ src/test/java/DiceGame/repository/PlayerRepositoryTest.java
@@ -18,8 +18,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
-@AutoConfigureDataMongo
-@DirtiesContext
 class PlayerRepositoryTest {
 
     @Autowired
